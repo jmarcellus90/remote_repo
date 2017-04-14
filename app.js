5dd19db20fc7e752d0d3ce021d8f9e33a05532ec
jmarcellus90 <jonanthanmarcellus@Jonanthans-MBP.scsu.southernct.edu>
2017-04-13 22:16:22 -0400

    new commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 //var users = require('./routes/users');
-var Robots = require('./routes/Robots');
+var Robots = require('./routes/robots');
 
 var app = express();
 
