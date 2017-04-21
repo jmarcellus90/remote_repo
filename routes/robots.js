bdbc28477bee0e3ca2b44bb1d1eb8bad2ea826bd
jmarcellus90 <jonanthanmarcellus@Jonanthans-MBP.scsu.southernct.edu>
2017-04-20 21:19:11 -0400

    get show page

--- routes/robots.js
+++ routes/robots.js
@@ -1,18 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var fetch = require('node-fetch');
 
-var myRobots = [
+/*var myRobots = [
   {"_id":"123abc", "name":"r2d2", "description":"holds a secret message", "in_stock":100},
   {"_id":"456def", "name":"bb8", "description":"rolls around", "in_stock":75},
   {"_id":"789xyz", "name":"c3po", "description":"specializes in language translation", "in_stock":50}
-] // static hard-coded data (for example)
+] // static hard-coded data (for example)*/
 
 /* List Robots */
 
 router.get('/robots', function(req, res, next) {
-  console.log("LISTING ROBOTS", myRobots)
-  res.render('robots/index', {robots: myRobots, title: "All Robots"});
-});
+  //console.log("LISTING ROBOTS", myRobots)
+  //
+    
+    var url = "https://southernct-443-robots-api.herokuapp.com/api/robots"
+
+        fetch(url)
+          .then(function(response) {
+            console.log("GOT A RESPONSE:", response)
+
+            response.json()
+              .then(function(json){
+                console.log("GOT SOME DATA:", json)
+                // DO SOMETHING WITH THE DATA HERE!
+                res.render('robots/index', {robots: json, title: "All Robots"});
+              })
+          })
+        });
 
 /* Show Robot */
 
